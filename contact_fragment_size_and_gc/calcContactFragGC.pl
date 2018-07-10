9d55360832c3da77b95164ff94e09a3d8d9814a1
shen <490788573@qq.com>
2018-07-10 14:34:23 +0800

    Create calcContactFragGC.pl

---
+++ contact_fragment_size_and_gc/calcContactFragGC.pl
@@ -0,0 +1,38 @@
+#arguement1 fasta
+#arguement2 contact fragment file
+
+
+my $fastaF = 'dm3.fa';
+
+#READING FASTA
+use Bio::Seq;
+use Bio::SeqIO;
+
+my $seqio = Bio::SeqIO->new(-file => $fastaF, '-format' => 'Fasta');
+my %fasta;
+while($seq = $seqio->next_seq){
+	my $id = $seq->display_name;
+	$fasta{$id} = $seq;
+}
+
+
+while(<>){
+    chomp;
+    my ($chr1, $sta1, $end1, $chr2, $sta2, $end2) = split "\t";
+    if (!$end1 || !$end2) {next;}
+    my $seq1 = $fasta{$chr1}->subseq($sta1,$end1);
+    my $seq2 = $fasta{$chr2}->subseq($sta2,$end2);
+    my $totalSeq = $seq1 . $seq2;
+    my $gc_ratio = &computeGC($totalSeq);
+    print $gc_ratio . "\n";
+}
+close FF;
+
+
+#function computeGC
+sub computeGC {
+	my $string = $_[0];
+	my $number = () = $string =~ /[g,c,G,C]/g;
+	my $ratio = $number/length($string);
+	return $ratio;
+}
